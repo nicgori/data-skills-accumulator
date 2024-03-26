4592a416c283cadd02bd98d6ed17377b007be108
nicgori <157612874+nicgori@users.noreply.github.com>
2024-03-26 16:32:37 +0000

    added matplotlib

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 import altair as alt
-
+import matplotlib.pyplot as plt
 
 # Page title
 st.set_page_config(page_title='Data Skillset Accumulator', page_icon='📊')
