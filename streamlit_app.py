ae508397b20e882c85bb12208804bd10bb565377
nicgori <157612874+nicgori@users.noreply.github.com>
2024-03-26 11:10:30 +0000

    Removed unneccassary stuff and added pathon as option default

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,34 +17,34 @@ st.subheader('Which Movie Genre performs ($) best at the box office?')
 
 # Load data
 df = pd.read_csv('data/skills_occurences_income.csv')
-df.year = df.year.astype('int')
+# df.year = df.year.astype('int')
 
 # Input widgets
 ## Genres selection
-genres_list = df.genre.unique()
-genres_selection = st.multiselect('Select genres', genres_list, ['Action', 'Adventure', 'Biography', 'Comedy', 'Drama', 'Horror'])
+skills_list = df.skill.unique()
+skills_selection = st.multiselect('Select skills', skills_list, ['python'])
 
-## Year selection
-year_list = df.year.unique()
-year_selection = st.slider('Select year duration', 1986, 2006, (2000, 2016))
-year_selection_list = list(np.arange(year_selection[0], year_selection[1]+1))
+# ## Year selection
+# year_list = df.year.unique()
+# year_selection = st.slider('Select year duration', 1986, 2006, (2000, 2016))
+# year_selection_list = list(np.arange(year_selection[0], year_selection[1]+1))
 
-df_selection = df[df.genre.isin(genres_selection) & df['year'].isin(year_selection_list)]
-reshaped_df = df_selection.pivot_table(index='year', columns='genre', values='gross', aggfunc='sum', fill_value=0)
-reshaped_df = reshaped_df.sort_values(by='year', ascending=False)
+# df_selection = df[df.genre.isin(genres_selection) & df['year'].isin(year_selection_list)]
+# reshaped_df = df_selection.pivot_table(index='year', columns='genre', values='gross', aggfunc='sum', fill_value=0)
+# reshaped_df = reshaped_df.sort_values(by='year', ascending=False)
 
 
 # Display DataFrame
 
-df_editor = st.data_editor(reshaped_df, height=212, use_container_width=True,
-                            column_config={"year": st.column_config.TextColumn("Year")},
+df_editor = st.data_editor(df, height=212, use_container_width=True,
+                            # column_config={"year": st.column_config.TextColumn("Year")},
                             num_rows="dynamic")
-df_chart = pd.melt(df_editor.reset_index(), id_vars='year', var_name='genre', value_name='gross')
+# df_chart = pd.melt(df_editor.reset_index(), id_vars='year', var_name='genre', value_name='gross')
 
 # Display chart
-chart = alt.Chart(df_chart).mark_line().encode(
-            x=alt.X('year:N', title='Year'),
-            y=alt.Y('gross:Q', title='Gross earnings ($)'),
-            color='genre:N'
-            ).properties(height=320)
-st.altair_chart(chart, use_container_width=True)
+# chart = alt.Chart(df_chart).mark_line().encode(
+#             x=alt.X('year:N', title='Year'),
+#             y=alt.Y('gross:Q', title='Gross earnings ($)'),
+#             color='genre:N'
+#             ).properties(height=320)
+# st.altair_chart(chart, use_container_width=True)
